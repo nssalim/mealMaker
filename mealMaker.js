91e33992526cadfa8d3c2005a16b060eae85123f
Nargis <nargis.salim@btinternet.com>
2021-07-01 19:04:12 +0100

    add menu object, courses properties and its getters

--- mealMaker.js
+++ mealMaker.js
@@ -1,6 +1,25 @@
 // Meal Maker
-// to randomly create a three-course meal based on what is available on a menu. Run programme until satisfied with the generated meal!
+// to randomly create a three-course meal based on what is available on any given menu. Run programme until satisfied with the generated meal!
 
 
-console.log('MEAL MAKER')
+// console.log('MEAL MAKER');
 
+
+const menu = {
+    // courses property to ultimately contain a mapping between each course and the dishes available to order in that course
+      _courses: {
+        appertizers: [],
+        mains: [],
+        desserts: [],
+      },
+      get appertizers(){
+        return this._courses.appetizers;
+      },
+      get mains(){
+        return this._courses.mains;
+      },
+      get desserts(){
+        return this._courses.deserts;
+      },
+    };
+    
