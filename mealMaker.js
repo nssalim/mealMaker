955c11f5b0f5186f5e80362258c80a33bf9272ca
Nargis <nargis.salim@btinternet.com>
2021-07-01 18:32:45 +0100

    first commit

---
+++ mealMaker.js
@@ -0,0 +1,6 @@
+// Meal Maker
+// to randomly create a three-course meal based on what is available on a menu. Run programme until satisfied with the generated meal!
+
+
+console.log('MEAL MAKER')
+
