fa56bcda76a788b6019d962694cd73092d7d6f9b
Nargis <nargis.salim@btinternet.com>
2021-07-02 04:15:22 +0100

    generate random meal and add menu items

--- mealMaker.js
+++ mealMaker.js
@@ -65,12 +65,56 @@ getRandomDishFromCourse(courseName){
   return dishes[randomIndex];
 },
 
-
+// automatically generate a three-course meal
+// function to create an appetizer variable which should be the result of calling the .getRandomDishFromCourse() method when passing in an appetizers string to it
+// repeat with appetizer and dessert
+// calculate total price and return a string that contains the name of each of the dishes and the total price of the meal
 generateRandomMeal(){
-  const appertizers = this.getRandomDishFrom('appetizers');
-    const mains = this.getRandomDishFrom('mains');
-      const desserts = this.getRandomDishFrom('desserts');
-      const totalPrice =  appetizer.price + main.price + dessert.price;
-      return `Your meal is ${appetizer.name}, ${main.name}, and ${dessert.name}, and the total price is ${totalPrice}`;
+  const appetizer = this.getRandomDishFromCourse('appetizers');
+  const main = this.getRandomDishFromCourse('mains');
+  const dessert = this.getRandomDishFromCourse('desserts');
+  const totalPrice =  appetizer.price + main.price + dessert.price;
+      return `\nDo you love trying out new restaurants and experimenting with different foods? \n\nDon't want to figure out what you want to order as it's become a time-consuming ordeal when the menu is too BIG? \n\nLook no further! \nWe randomly create a three-course meal based on what is available on any given menu! \n\nWhy not try:-  \nStarter:  ${appetizer.name} \nMain:  ${main.name} \nDessert:  ${dessert.name}  \nTotal price:- £${totalPrice}\n\nA quick and easy way to seek and find a food combination to love!`;
   }
 };
+
+
+// add appetizers, mains and desserts
+menu.addDishToCourse('appetizers', 'vegetable samosas', 3.50);
+menu.addDishToCourse('appetizers', 'pakoras', 3.50);
+menu.addDishToCourse('appetizers', 'spiced mushrooms', 3.50);
+
+
+menu.addDishToCourse('mains', 'Tarka Daal', 5.25);
+menu.addDishToCourse('mains', 'palak paneer', 6.50);
+menu.addDishToCourse('mains', 'biryani', 8.50);
+
+
+menu.addDishToCourse('desserts', 'Ras malai', 3.00);
+menu.addDishToCourse('desserts', 'Kheer', 4.50);
+menu.addDishToCourse('desserts', 'Gulab Jamun', 3.50);
+menu.addDishToCourse('desserts', 'Jalebi', 4.00);
+
+
+// generate a three-course meal
+// print generated meal
+const meal = menu.generateRandomMeal();
+console.log(meal);
+// Output:
+// MEAL MAKER
+
+// Do you love trying out new restaurants and experimenting with different foods? 
+
+// Don't want to figure out what you want to order as it's become a time-consuming ordeal when the menu is too BIG? 
+
+// Look no further! 
+// We randomly create a three-course meal based on what is available on any given menu! 
+
+// Why not try:-  
+// Starter:  spiced mushrooms 
+// Main:  Tarka Daal 
+// Dessert:  Ras malai  
+// Total price:- £11.75
+
+// A quick and easy way to seek and find a food combination to love!
+
